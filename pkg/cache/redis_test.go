d9574f1fee4d856770d5cfaaa2e3806ad2cf335c
Abolfazl Alizadeh <mrabolfazlalz@gmail.com>
2023-06-07 09:05:35 +0330

    Update redis_test.go

--- pkg/cache/redis_test.go
+++ pkg/cache/redis_test.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -23,17 +24,21 @@ func TestRedis_Set(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	value := &User{}
-	if err := cache.Get("test", value); err != nil {
+	var value string
+	if err := cache.Get("test", &value); err != nil {
 		if err != nil {
 			t.Fatal(err)
 			return
 		}
 		t.Fatalf("value must be 'value' instead is `%s`", value)
 	}
+
+	TestRedis_Set2(t)
 }
 
 func TestRedis_Forget(t *testing.T) {
+	fmt.Println("T")
+
 	value := "value"
 	key := "test"
 	if err := cache.Set(Item{key, value, time.Second * 1}); err != nil {
@@ -54,21 +59,24 @@ func TestRedis_Forget(t *testing.T) {
 	}
 }
 
-func TestStructCache(t *testing.T) {
+func TestRedis_Set2(t *testing.T) {
+	log.Println("fuck")
 	key := "struct_cache"
 	value := User{Name: "Abolfazl", Lastname: "Alizadeh"}
-	if err := cache.Set(Item{key, value, time.Second * 1}); err != nil {
+	if err := cache.Set(Item{key, &value, time.Second * 1}); err != nil {
 		log.Fatal(err)
 	}
 
 	var u User
+	//var u map[string]string
+
 	err := cache.Get(key, &u)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if u.Lastname != value.Lastname && u.Name != value.Name {
+	if u.Lastname != value.Lastname || u.Name != value.Name {
 		log.Fatal("Name and Lastname User in cache does not match")
 	}
 
